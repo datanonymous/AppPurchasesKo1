d2cc35d1f5f592e7320cd89f75d8561503131b91
Alex Ko <ko.e.alexander@gmail.com>
2019-08-09 21:32:02 -0400

    Initial commit

---
+++ app/src/main/java/ko/alex/apppurchasesko1/MainActivity.java
@@ -0,0 +1,21 @@
+package ko.alex.apppurchasesko1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //https://developer.squareup.com/docs/in-app-payments-sdk/quick-start/step-1
+    //https://developer.squareup.com/docs/in-app-payments-sdk/quick-start/step-2
+    //https://developer.squareup.com/docs/in-app-payments-sdk/quick-start/step-3
+    //https://developer.squareup.com/docs/in-app-payments-sdk/quick-start/step-4
+    //https://developer.squareup.com/docs/in-app-payments-sdk/quick-start/step-5
+    //https://developer.squareup.com/docs/in-app-payments-sdk/installation
+    //https://developer.squareup.com/docs/in-app-payments-sdk/build-on-android
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
